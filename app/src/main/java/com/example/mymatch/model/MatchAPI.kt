a5d60ce1680460de0ef4cc5662908fb999107be4
elisa.roger <e26.roger@gmail.com>
2024-03-28 10:54:49 +0100

    Ajout add1Point dans API

--- app/src/main/java/com/example/mymatch/model/MatchAPI.kt
+++ app/src/main/java/com/example/mymatch/model/MatchAPI.kt
@@ -1,19 +1,29 @@
 package com.example.mymatch.model
 
 import com.example.mymatch.beans.MatchBean
+import com.example.mymatch.model.MatchAPI.add1Point
 import com.example.mymatch.model.MatchAPI.createMatch
+import com.example.mymatch.model.MatchAPI.load7DayzMatch
 import com.google.gson.Gson
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
+
 
 fun main() {
+    //Création de la date du jour
+    val currentTimeMillis = System.currentTimeMillis()
+    //Créer un match
+    createMatch("MHB","Nantes",currentTimeMillis)
+    //lister les match
+    val listest = load7DayzMatch()
 
-    createMatch("PSG","Lyon",51984651)
-
-
+    //ajouter un point
+    listest[0].id?.let { add1Point(it, 1) }
+    println(listest)
 
 }
 
@@ -22,16 +32,18 @@ object MatchAPI {
     val MEDIA_TYPE_JSON = "application/json; charset=utf-8".toMediaType()
     val gson = Gson()
     val client = OkHttpClient()
-    private const val URL_SERVER = "http://2.9.163.31:8080"
-
+    private const val URL_SERVER = "http://localhost:8080"
+//"http://2.9.163.31:8080"   http://localhost:8080
     fun createMatch(equipe1 : String, equipe2 : String, date: Long) {
-
-        val res = sendPost("$URL_SERVER/mymatch/creatematch", MatchBean(null, date, equipe1, equipe2))
-
+        val res = sendPost("$URL_SERVER/mymatch/creatematch", MatchBean(null, date, equipe1, equipe2, status = true))
         println(res)
 
     }
 
+    fun add1Point(idMatch: Long, equipe: Int): MatchBean {
+        val responseJson = sendPostaddPoint("$URL_SERVER/mymatch/score", idMatch, equipe)
+        return gson.fromJson(responseJson, MatchBean::class.java)
+    }
 
     fun load7DayzMatch(): List<MatchBean> {
         var json = sendGet("$URL_SERVER/mymatch/7dayz")
@@ -52,20 +64,43 @@ object MatchAPI {
             it.body.string()
         }
     }
-    fun sendPost(url: String, toSend: Any?): String {
-        println("url : $url")
+        fun sendPost(url: String, toSend: Any?): String {
+            println("url : $url")
 
-        val json = gson.toJson(toSend)
+            val json = gson.toJson(toSend)
 
-        val body = json.toRequestBody(MEDIA_TYPE_JSON)
-        val request = Request.Builder().url(url).post(body).build()
+            val body = json.toRequestBody(MEDIA_TYPE_JSON)
+            val request = Request.Builder().url(url).post(body).build()
 
-        return client.newCall(request).execute().use {
-            if (!it.isSuccessful) {
-                throw Exception("Réponse du serveur incorrect :${it.code}")
+            return client.newCall(request).execute().use {
+                if (!it.isSuccessful) {
+                    throw Exception("Réponse du serveur incorrect :${it.code}")
+                }
+                it.body.string()
             }
-            it.body.string()
+        }
+
+    fun sendPostaddPoint(url: String, idMatch: Long, equipe: Int): String {
+        println("URL : $url")
+
+        val requestUrl = url.toHttpUrlOrNull()!!.newBuilder()
+            .addQueryParameter("idMatch", idMatch.toString())
+            .addQueryParameter("equipe", equipe.toString())
+            .build()
+
+        val request = Request.Builder()
+            .url(requestUrl)
+            .post(RequestBody.create(null, ""))
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+        val response = client.newCall(request).execute()
+        return response.use {
+            if (!it.isSuccessful) {
+                throw Exception("Réponse du serveur incorrect : ${it.code}")
+            }
+            it.body!!.string()
         }
     }
 
-    }
+}
